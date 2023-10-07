d0a77eabe487a9327b2fcddde8d367ef99ff32d2
Lanna Vieira <lanna_148@hotmail.com>
2023-10-06 21:44:22 -0300

    exercício num 2 resolvido - estrutura sequencial

--- estrutura_sequencial/002_media_bimestral.py
+++ estrutura_sequencial/002_media_bimestral.py
@@ -1,1 +1,15 @@
 # Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+#int = conversão de uma variável ou string para um número inteiro
+#float = transforma um dado em um tipo flutuante, ou seja, um número com casa decimal
+primeira_nota = int(input("Infome sua nota 1: "))
+segunda_nota = float(input("Infome sua nota 2: "))
+terceira_nota = int(input("Infome sua nota 3: "))
+quarta_nota = float(input("Infome sua nota 4: "))
+
+media_bimestral = (primeira_nota + segunda_nota + terceira_nota + quarta_nota) / 4
+
+# f = formate = quando voce usa o "f" antes de uma sting, significa que voce está permitindo a formação da mesma, ou seja, usar variáveis ou alterar parâmetros.
+# :.2f = formatação = remove as casas extras após a virgula
+print(f"Sua média bimestral é {media_bimestral:.1f}")
+
