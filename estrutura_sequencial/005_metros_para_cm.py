e2ed2176e24fd7697cf1ccad46c756ecb2af908d
Lanna Vieira <lanna_148@hotmail.com>
2023-10-09 21:57:44 -0300

    finalizado exercicio 005 - estrutura sequencial

--- estrutura_sequencial/005_metros_para_cm.py
+++ estrutura_sequencial/005_metros_para_cm.py
@@ -1,1 +1,11 @@
 # Faça um Programa que converta metros para centímetros.
+
+metros = float(input("Informe o metro: "))
+
+conversao = 100
+
+m_para_cm = metros * conversao
+
+print(f"A converão de {metros:.2f}m para centímetros é: {m_para_cm:.0f}cm")
+
+
