922370ef4835c5195fecc7885302128478198a09
Lanna Vieira <lanna_148@hotmail.com>
2023-10-09 21:44:51 -0300

    finalizado exercicio 004 - estrutura sequencial

--- estrutura_sequencial/004_soma.py
+++ estrutura_sequencial/004_soma.py
@@ -1,1 +1,10 @@
 # Faça um Programa que peça dois números e imprima a soma.
+
+numero_um = float(input("Informe um número:"))
+numero_dois = float(input("Informe outro número:"))
+
+soma = numero_um + numero_dois
+
+print(f"Soma dos números é {soma}")
+
+# Outra forma de realizar equação : print(f"Soma dos números é {numero_um + numero_dois}")
