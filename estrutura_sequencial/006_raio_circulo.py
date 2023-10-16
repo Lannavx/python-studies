b63dac6804544845624e538ddad1fc32f0a8fc7e
Lanna Vieira <lanna_148@hotmail.com>
2023-10-16 20:17:41 -0300

    finalizado exercicio 006 - estrutura sequencial

--- estrutura_sequencial/006_raio_circulo.py
+++ estrutura_sequencial/006_raio_circulo.py
@@ -1,2 +1,10 @@
 # Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
 
+# Sempre que falamos de números quebrados, usamos "."
+numero_de_pi = 3.14159
+
+raio = float(input("Informe o raio de um círculo: "))
+
+a = numero_de_pi * (raio**2)
+
+print(f"O cálculo da área é: {a:.3f}")
