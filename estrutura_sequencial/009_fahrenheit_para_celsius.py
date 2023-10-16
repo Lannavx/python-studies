68c6f76040deaae30b2bd0bf727335e11b5736da
Lanna Vieira <lanna_148@hotmail.com>
2023-10-16 20:53:54 -0300

    finalizado exercicio 009 - estrutura sequencial

--- estrutura_sequencial/009_fahrenheit_para_celsius.py
+++ estrutura_sequencial/009_fahrenheit_para_celsius.py
@@ -4,3 +4,9 @@
     Fórmula: C = 5 * ((F-32) / 9).
 
 """
+
+fahrenheit = float(input("Informe a temperatura em F: "))
+
+conversao_celsius = 5 * ((fahrenheit - 32) / 9)
+
+print(f"Sua temperatura em graus Celsius é: {conversao_celsius:.1f}")
