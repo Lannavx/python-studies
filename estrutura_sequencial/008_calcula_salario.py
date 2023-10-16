94473640bfb2ffaca2ccd06c3c1b8e8a69e891cd
Lanna Vieira <lanna_148@hotmail.com>
2023-10-16 20:46:16 -0300

    finalizado exercicio 008 - estrutura sequencial

--- estrutura_sequencial/008_calcula_salario.py
+++ estrutura_sequencial/008_calcula_salario.py
@@ -1,1 +1,11 @@
 # Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
+mes = str(input("Informe o mês: "))
+ganho_por_hora = float(input("Informe quanto você ganha por hora: "))
+horas_trabalhadas = float(input(f"Informe o número das suas horas trabalhadas no mês de {mes}: "))
+ 
+
+total_salario = ganho_por_hora * horas_trabalhadas
+
+print(f"Seu salário total no mês de {mes} é: R${total_salario:.2F}")
+
