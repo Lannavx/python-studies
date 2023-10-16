468a313926838d58a806c31f0f2630a9ab3f3ed8
Lanna Vieira <lanna_148@hotmail.com>
2023-10-16 20:27:51 -0300

    finalizado exercicio 007 - estrutura sequencial

--- estrutura_sequencial/007_area_quadrado.py
+++ estrutura_sequencial/007_area_quadrado.py
@@ -1,1 +1,9 @@
 # Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+lado_quadrado = float(input("Informe o tamanho dos lados do quadrado: "))
+
+area_do_quadrado = (lado_quadrado ** 2)
+dobro_do_quadrado = area_do_quadrado * 2
+
+print(f"O dobro da área do quadrado é: {dobro_do_quadrado:.2f}")
+
