afa8821905396f2429f18ef809c0c08686fc9584
Lanna Vieira <lanna_148@hotmail.com>
2023-10-16 21:01:15 -0300

    finalizado exercicio 010 - estrutura sequencial

--- estrutura_sequencial/010_celsius_para_fahrenheit.py
+++ estrutura_sequencial/010_celsius_para_fahrenheit.py
@@ -1,1 +1,8 @@
 # Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+# Fórmula = (0 °C × 9/5) + 32 = 32 °F
+
+celsius = float(input("Informe a temperatura em C: "))
+
+conversao_celsius = (celsius * (9/5) + 32)
+
+print(f"Sua temperatura em Fahrenheit é: {conversao_celsius:.1f} °F")
