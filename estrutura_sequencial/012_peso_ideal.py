c3edc003e6be360c5655a3990083e25c7a4256f7
Lanna Vieira <lanna_148@hotmail.com>
2023-10-25 17:04:39 -0300

    finalizado exercicio 012 - estrutura sequencial

--- estrutura_sequencial/012_peso_ideal.py
+++ estrutura_sequencial/012_peso_ideal.py
@@ -2,3 +2,9 @@
     Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal.
     Fórmula: (72.7*altura) - 58
 """
+
+altura = float(input('Informe a sua altura: '))
+
+peso_ideal = (72.7*altura) - 58
+
+print(f'O seu peso ideal baseado na sua altura é {peso_ideal:.2f}')
