b03306afaa528e33c7005857ab5ee88c6c3d6578
Lanna Vieira <lanna_148@hotmail.com>
2023-11-30 19:19:43 -0300

    finalizado exercicio 013 - estrutura sequencial

--- estrutura_sequencial/013_peso_ideal_avancado.py
+++ estrutura_sequencial/013_peso_ideal_avancado.py
@@ -3,3 +3,10 @@
         1.Para homens: (72.7*h) - 58
         2.Para mulheres: (62.1*h) - 44.7
 """
+
+genero = input(' ')
+altura = float(input('Informe sua altura: '))
+
+para_mulheres = (62.1*altura) - 44.7
+para_homens = (72.7*altura) - 58
+
