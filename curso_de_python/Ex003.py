6188ad66932bc6367927a5ee7aa24bdf2e2beb34
Lanna Vieira <lanna_148@hotmail.com>
2023-12-10 10:32:22 -0300

    Finalizado Ex3

---
+++ curso_de_python/Ex003.py
@@ -0,0 +1,9 @@
+#Crie um programa que leia dois números e mostre a soma entre eles
+
+n1 = int(input('Insira um número: '))
+n2 = int(input('Insira outro número: '))
+
+s = n1 + n2
+
+print(f'A soma de {n1} + {n2} é {s}')
+
