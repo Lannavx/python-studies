f7d0cac578605708f99f885f48e63c190951be6c
Lanna Vieira <lanna_148@hotmail.com>
2023-12-10 10:36:05 -0300

    Finalizado Ex9

---
+++ curso_de_python/Ex009.py
@@ -0,0 +1,14 @@
+#Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada
+
+n1 = int(input('Digite um número para ver a sua tabuada: '))
+
+print(f'{n1} X 1 = {(n1*1):2}')
+print(f'{n1} X 2 = {(n1*2):2}')
+print(f'{n1} X 3 = {(n1*3):2}')
+print(f'{n1} X 4 = {(n1*4):2}')
+print(f'{n1} X 5 = {(n1*5):2}')
+print(f'{n1} X 6 = {(n1*6):2}')
+print(f'{n1} X 7 = {(n1*7):2}')
+print(f'{n1} X 8 = {(n1*8):2}')
+print(f'{n1} X 9 = {(n1*9):2}')
+print(f'{n1} X 10 = {(n1*10):2}')
