ed7aaaaa620e5de8514a18639ab025a2c1a65ff1
Lanna Vieira <lanna_148@hotmail.com>
2023-12-12 08:58:27 -0300

    Finalizado Ex26

---
+++ curso_de_python/Ex026.py
@@ -0,0 +1,17 @@
+'''Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", 
+em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.'''
+
+#Forma 1
+
+frase = str(input('Informe uma frase qualquer: ')).strip().upper()
+vezes = frase.count('A')
+posição_1 = frase.find('A')
+posição_2 = frase.rfind('A')
+print(f'A letra A aparece {vezes} vezes na frase')
+print(f'A primeira posição da letra A é {posição_1+1} e a última é {posição_2+1}')
+
+#Forma 2
+
+frase = str(input('Informe uma frase qualquer: ')).strip().upper()
+print(f'A letra A aparece {frase.count('A')} vezes na frase')
+print(f'A primeira posição da letra A é {frase.find('A')+1} e a última é {frase.rfind('A')+1}')
