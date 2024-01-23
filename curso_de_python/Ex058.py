b391cbab255f15a14b8fa8b7abc9a47afb5fb64d
Lanna Vieira <lanna_148@hotmail.com>
2024-01-23 16:57:13 -0300

    Finalizado Ex58

---
+++ curso_de_python/Ex058.py
@@ -0,0 +1,34 @@
+'''Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em um número entre 0 e 10. 
+Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.'''
+
+
+palpite = 1
+from random import randint    
+print('Ei, vamos jogar um jogo! Vou pensar em um número entre 0 e 10 e você precisa adinvinhar qual é!')
+print('Valendoo!')
+numero_secreto = randint(0,10)
+numero_usuário = int(input('Informe o seu palpite: '))
+while numero_usuário != numero_secreto:
+    numero_usuário = int(input('Você errou! Tente novamente: '))
+    palpite += 1
+print(f'Parabéns, você acertou! Foram necessárias {palpite} tentativas!')
+
+#Outra forma
+
+from random import randint    
+print('Ei, vamos jogar um jogo! Vou pensar em um número entre 0 e 10 e você precisa adinvinhar qual é!')
+print('Valendoo!')
+numero_secreto = randint(0,10)
+acertou = False
+palpite = 0
+while not acertou:
+    numero_usuário = int(input('Informe o seu palpite: '))
+    palpite += 1
+    if numero_usuário == numero_secreto:
+        acertou = True
+    else:
+        if numero_usuário < numero_secreto:
+            print('Mais...tente novamente!')
+        elif numero_usuário > numero_secreto:
+            print('Menos...tente novamente!')   
+print(f'Parabéns, você acertou! Foram necessárias {palpite} tentativas!')
