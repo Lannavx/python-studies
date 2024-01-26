3f799f159469940c20f43b8dcd7fcae33335356d
Lanna Vieira <lanna_148@hotmail.com>
2024-01-25 21:35:52 -0300

    finalizado exercicio 018 - estrutura sequencial

--- estrutura_sequencial/018_velocidade_de_download.py
+++ estrutura_sequencial/018_velocidade_de_download.py
@@ -3,3 +3,12 @@
     e a velocidade de um link de Internet (em Mbps), calcule e informe o 
     tempo aproximado de download do arquivo usando este link (em minutos).
 """
+
+tamanho = float(input('Informe o tamanho do seu arquivo para download: '))
+velocidade = int(input('Informe a velocidade do link: '))
+
+#calculo do tempo de download
+#converte a velocidade de Mbps para MBps e calcula o tempo em minutos
+
+tempo_download = tamanho / (velocidade / 8 ) / 60
+print(f'O tempo aproximado de download do arquivo é de {tempo_download:.2f} minutos')
