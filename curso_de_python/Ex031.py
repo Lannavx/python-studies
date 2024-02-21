9a50879cd448e6e186df2cc14cf0b462f19caa93
Lanna Vieira <lanna_148@hotmail.com>
2024-02-21 13:26:03 -0300

    Exercício refatorado para nomenclatura convencional de variáveis

--- curso_de_python/Ex031.py
+++ curso_de_python/Ex031.py
@@ -4,25 +4,25 @@ cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais lo
 #Forma 1
 distancia = float(input('Qual a distância da viagem em km? '))   
 if distancia <= 200:
-   preço = distancia * 0.50
-   print(f'Sua viagem é {distancia}km, logo sua passagem custa R${preço:.2f}')
+   preco = distancia * 0.50
+   print(f'Sua viagem é {distancia}km, logo sua passagem custa R${preco:.2f}')
 else:
-   preço = distancia * 0.45
-   print(f'Sua viagem é {distancia}km, logo sua passagem custa R${preço:.2f}')
+   preco = distancia * 0.45
+   print(f'Sua viagem é {distancia}km, logo sua passagem custa R${preco:.2f}')
 
 #Forma 2   
 
 distancia = float(input('Qual a distância da viagem em km? '))
 print(f'Você está prestes a começar uma viagem de {distancia}km,')          
 if distancia <= 200:
-   preço = distancia * 0.50
+   preco = distancia * 0.50
 else:
-   preço = distancia * 0.45
-print(f'E o preço da sua passagem será de R${preço:.2f}')
+   preco = distancia * 0.45
+print(f'E o preço da sua passagem será de R${preco:.2f}')
 
 #Forma 3 - If simplificado
 
 distancia = float(input('Qual a distância da viagem em km? '))
 print(f'Você está prestes a começar uma viagem de {distancia}km,')          
-preço = distancia * 0.50 if distancia <= 200 else distancia * 0.45
-print(f'E o preço da sua passagem será de R${preço:.2f}')
+preco = distancia * 0.50 if distancia <= 200 else distancia * 0.45
+print(f'E o preço da sua passagem será de R${preco:.2f}')
