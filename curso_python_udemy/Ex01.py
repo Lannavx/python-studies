b2692a5ee05293b164aa91c11cd85d092cdaf969
Lanna Vieira <lanna_148@hotmail.com>
2024-03-20 11:41:59 -0300

    Finalizado Ex01

---
+++ curso_python_udemy/Ex01.py
@@ -0,0 +1,15 @@
+# Aula de variáveis e tipos de dados
+
+nome = 'Ana'
+sobrenome = 'Castro'
+idade = 45
+ano_nascimento = 2024 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.77
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
