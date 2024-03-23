3e193bf18b62e7528485a18e515c5156c50c947d
Lanna Vieira <lanna_148@hotmail.com>
2024-03-23 09:16:12 -0300

    Finalizado Ex02

---
+++ curso_python_udemy/Ex02.py
@@ -0,0 +1,8 @@
+# Cálculo do IMC 
+
+nome = 'Ana Castro'
+altura = 1.77
+peso = 83
+imc = peso / (altura * altura)
+
+print(nome,'tem', altura, 'de altura,', 'pesa', peso, 'quilos e seu IMC é', imc)
