f75a70bf3714dc8691aafbe5348d312a52a745a5
Lanna Vieira <lanna_148@hotmail.com>
2024-05-24 17:45:55 -0300

    Finalizado Ex010

--- ex_python_brasil/2_estrutura_de_decisao/010_saudacao_por_turno.py
+++ ex_python_brasil/2_estrutura_de_decisao/010_saudacao_por_turno.py
@@ -1,2 +1,14 @@
 '''Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. 
 Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.'''
+
+
+turno = input('Informe o turno que você estuda: Matutino, Vespertino ou Noturno [M/V/N]: ').strip().upper()[0]
+
+if turno == 'M':
+    print('Bom Dia!')
+elif turno == 'V':
+    print('Boa Tarde!')
+elif turno == 'N':
+    print('Boa Noite!')   
+else:
+    print('Valor Inválido! Informe o turno que você estuda: Matutino, Vespertino ou Noturno. ')        
