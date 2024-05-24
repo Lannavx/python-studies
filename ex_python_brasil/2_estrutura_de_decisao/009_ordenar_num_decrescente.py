d6408171455534d80f09fb34991cc5ca93fb0dc3
Lanna Vieira <lanna_148@hotmail.com>
2024-05-24 17:34:01 -0300

    Finalizado Ex009

--- ex_python_brasil/2_estrutura_de_decisao/009_ordenar_num_decrescente.py
+++ ex_python_brasil/2_estrutura_de_decisao/009_ordenar_num_decrescente.py
@@ -1,1 +1,13 @@
 # Faça um Programa que leia três números e mostre-os em ordem decrescente.
+
+numero_1 = int(input('Informe um número: '))
+numero_2 = int(input('Informe mais um número: '))
+numero_3 = int(input('Informe outro número: '))
+
+# Cria uma lista com os números informados
+lista_numeros = [numero_1, numero_2, numero_3]
+
+# Ordena a lista em ordem decrescente com o parâmetro reverse=True
+lista_numeros.sort(reverse=True)
+
+print(lista_numeros)
