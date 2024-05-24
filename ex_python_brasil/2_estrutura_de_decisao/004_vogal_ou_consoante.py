f6c74e01bc0e7aeeae501a886812574669f375f5
Lanna Vieira <lanna_148@hotmail.com>
2024-05-24 14:37:29 -0300

    Finalizado Ex004

--- ex_python_brasil/2_estrutura_de_decisao/004_vogal_ou_consoante.py
+++ ex_python_brasil/2_estrutura_de_decisao/004_vogal_ou_consoante.py
@@ -1,1 +1,14 @@
 # Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+
+letra = input('Digite qualquer letra: ').lower().strip()
+
+lista_vogal = ['a', 'e', 'i', 'o', 'u']
+
+if len(letra) == 1 and letra.isalpha(): # isalpha verifica se a entrada é uma letra do alfabeto.
+    if letra in lista_vogal:
+        print('Sua letra é uma vogal!')
+    else:
+        print('Sua letra é uma consoante!')
+else:
+    print('Entrada inválida! Digite uma única uma letra.')            
