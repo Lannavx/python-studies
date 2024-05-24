98b5f6a36dd77b87f23d3be36b6d1968909ea937
Lanna Vieira <lanna_148@hotmail.com>
2024-05-24 14:36:09 -0300

    Finalizado Ex003

--- ex_python_brasil/2_estrutura_de_decisao/003_classificacao_genero.py
+++ ex_python_brasil/2_estrutura_de_decisao/003_classificacao_genero.py
@@ -1,4 +1,13 @@
 '''
 Faça um Programa que verifique se uma letra digitada é "F" ou "M". 
-Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+Conforme a letra, escreva: F - Feminino, M - Masculino, Sexo Inválido.
 '''
+
+sexo = input('Informe seu sexo [F/M]: ').upper().strip()
+
+if sexo == 'F':
+    print('F - Feminino')
+elif sexo == 'M':
+    print('M - Masculino')    
+else:
+    print('Sexo inválido!')    
