8ba5ed42782db3a4a1ad22994b5f8d220eb078ef
Lanna Vieira <lanna_148@hotmail.com>
2024-05-24 13:18:30 -0300

    Finalizado Ex001

--- ex_python_brasil/2_estrutura_de_decisao/001_maximo_de_dois.py
+++ ex_python_brasil/2_estrutura_de_decisao/001_maximo_de_dois.py
@@ -1,1 +1,9 @@
 # Faça um Programa que peça dois números e imprima o maior deles.
+
+numero_1 = int(input('Informe um número: '))
+numero_2 = int(input('Informe outro número: '))
+
+if numero_1 > numero_2:
+    print(f'O maior número é: {numero_1}')
+else:
+    print(f'O maior número é: {numero_2}')
