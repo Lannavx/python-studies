86f8afe31448a86f9acf89b81a3ec0d427e6c21b
Lanna Vieira <lanna_148@hotmail.com>
2024-05-24 14:47:12 -0300

    Finalizado Ex005

--- ex_python_brasil/2_estrutura_de_decisao/005_sistema_notas_aluno.py
+++ ex_python_brasil/2_estrutura_de_decisao/005_sistema_notas_aluno.py
@@ -4,3 +4,15 @@ O programa deve calcular a média alcançada por aluno e apresentar:
 A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
 A mensagem "Reprovado", se a média for menor do que sete;
 A mensagem "Aprovado com Distinção", se a média for igual a dez.'''
+
+nota_1 = float(input('Informe sua primeira nota: '))
+nota_2 = float(input('Informe sua segunda nota: '))
+
+media = (nota_1 + nota_2) / 2
+
+if media == 10:
+    print(f'Sua média foi {media}. Parabéns, você foi aprovado com distinção!')
+elif media >= 7:
+    print(f'Sua média foi {media}. Parabéns, você foi aprovado!')
+else:
+    print(f'Sua média foi {media}. Você está reprovado!')        
