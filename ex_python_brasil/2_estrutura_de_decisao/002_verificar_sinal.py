c9ce469777a64d785cff5bf7051ebcfee1ee87c2
Lanna Vieira <lanna_148@hotmail.com>
2024-05-24 13:36:52 -0300

    Finalizado Ex002

--- ex_python_brasil/2_estrutura_de_decisao/002_verificar_sinal.py
+++ ex_python_brasil/2_estrutura_de_decisao/002_verificar_sinal.py
@@ -1,1 +1,10 @@
 # Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+numero = int(input('Informe um número: '))
+
+if numero > 0:
+    print(f'{numero} é positivo!')
+elif numero < 0:
+    print(f'{numero} é negativo!')
+else:
+    print('O número é zero!')
