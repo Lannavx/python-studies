2b0421b87b81e12a3ae48eb65fda203ea3c5855e
Lanna Vieira <lanna_148@hotmail.com>
2024-05-26 00:16:47 -0300

    Finalizado Ex013

--- ex_python_brasil/2_estrutura_de_decisao/013_dia_da_semana.py
+++ ex_python_brasil/2_estrutura_de_decisao/013_dia_da_semana.py
@@ -1,2 +1,22 @@
 ''' Faça um Programa que leia um número e exiba o dia correspondente da semana.
 (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.'''
+
+numero_semana = int(input('Informe um número para exibir o dia da semana correspondente (1-Domingo, 2-Segunda, etc.): '))
+
+if numero_semana == 1:
+    print('Domingo')
+elif numero_semana == 2:
+    print('Segunda-feira')
+elif numero_semana == 3:
+    print('Terça-feira')
+elif numero_semana == 4:
+    print('Quarta-feira')    
+elif numero_semana == 5:
+    print('Quinta-feira')
+elif numero_semana == 6:
+    print('Sexta-feira')
+elif numero_semana == 7:
+    print('Sábado')
+else:
+    print('Valor inválido! Digite um número entre e 1 e 7.')    
+       
