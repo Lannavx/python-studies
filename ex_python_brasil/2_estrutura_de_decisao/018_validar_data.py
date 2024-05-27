ef0ce5282e098d82108ca205bb77920ff43e7ce3
Lanna Vieira <lanna_148@hotmail.com>
2024-05-27 16:17:09 -0300

    Finalizado Ex018

--- ex_python_brasil/2_estrutura_de_decisao/018_validar_data.py
+++ ex_python_brasil/2_estrutura_de_decisao/018_validar_data.py
@@ -1,1 +1,14 @@
 # Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesma é uma data válida.
+
+from datetime import datetime
+
+data = input('Informe uma data no formato dd/mm/aaaa: ')
+
+try:
+    data_valida = datetime.strptime(data, '%d/%m/%Y')
+    print('Data no formato correto e válida!')
+except ValueError:
+    print('Formato errado ou data inválida! Digite uma data no formato dd/mm/aaaa')
+
+
+
