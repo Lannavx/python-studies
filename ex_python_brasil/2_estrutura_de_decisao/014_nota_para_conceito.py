5b8fb76005dea710979cec5410b2f061445f9360
Lanna Vieira <lanna_148@hotmail.com>
2024-05-26 21:25:53 -0300

    Finalizado Ex014

--- ex_python_brasil/2_estrutura_de_decisao/014_nota_para_conceito.py
+++ ex_python_brasil/2_estrutura_de_decisao/014_nota_para_conceito.py
@@ -10,3 +10,39 @@ A atribuição de conceitos obedece à tabela abaixo:
  O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente e a mensagem “APROVADO” 
  se o conceito for A, B ou C ou “REPROVADO” se o conceito for D ou E. 
  '''
+
+nota_um = float(input('Informe a primeira nota: '))
+nota_dois = float(input('Informe a segunda nota: '))
+
+if 0 <= nota_um <= 10 and 0 <= nota_dois <= 10:
+    media = (nota_um + nota_dois) / 2
+
+    if 0 <= media < 4:
+        conceito = 'E'
+        status = 'REPROVADO'
+    elif 4 <= media < 6:
+        conceito = 'D'
+        status = 'REPROVADO'
+    elif 6 <= media < 7.5:
+        conceito = 'C'
+        status = 'APROVADO'    
+    elif 7.5 <= media < 9:
+        conceito = 'B'
+        status = 'APROVADO'
+    elif 9 <= media <= 10:
+        conceito = 'A'
+        status = 'APROVADO'
+    else:
+        conceito = 'Inválido'
+        status = 'Inválido'
+
+    print('=' * 20)
+    print(f'''
+    Primeira Nota: {nota_um}
+    Segunda Nota: {nota_dois}
+    Média: {media:.2f}
+    Conceito: {conceito}
+    Status: {status}
+    ''')
+else:
+    print('Notas inválidas. As notas devem estar entre 0 e 10.')
