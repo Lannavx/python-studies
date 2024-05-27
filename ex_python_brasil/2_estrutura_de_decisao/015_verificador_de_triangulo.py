329f92dd28b2a59e5a741fee712db362908164a2
Lanna Vieira <lanna_148@hotmail.com>
2024-05-27 12:00:05 -0300

    Finalizado Ex015

--- ex_python_brasil/2_estrutura_de_decisao/015_verificador_de_triangulo.py
+++ ex_python_brasil/2_estrutura_de_decisao/015_verificador_de_triangulo.py
@@ -6,3 +6,25 @@ Dicas:
     Triângulo Isósceles: quaisquer dois lados iguais;
     Triângulo Escaleno: três lados diferentes;
 '''
+
+print('Informe os 3 lados de um triângulo: ')
+primeiro_lado = float(input('Primeiro lado: '))
+segundo_lado = float(input('Segundo lado: '))
+terceiro_lado = float(input('Terceiro lado: '))
+
+# Verifica se os lados podem formar um triângulo
+if (primeiro_lado + segundo_lado > terceiro_lado and
+    primeiro_lado + terceiro_lado > segundo_lado and
+    segundo_lado + terceiro_lado > primeiro_lado):
+    
+    # Classifica os triângulos
+    if primeiro_lado == segundo_lado and segundo_lado == terceiro_lado:
+        print('Seu triângulo é um Triângulo Equilátero!')
+    elif (primeiro_lado == segundo_lado or 
+          segundo_lado == terceiro_lado or 
+          primeiro_lado == terceiro_lado):
+        print('Seu triângulo é um Triângulo Isósceles!')
+    else:
+        print('Seu triângulo é um Triângulo Escaleno!')
+else:
+    print('Os valores informados não podem formar um triângulo!') 
