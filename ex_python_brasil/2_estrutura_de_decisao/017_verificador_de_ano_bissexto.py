2c19e4df2674029e334ecb822081e30c7349d894
Lanna Vieira <lanna_148@hotmail.com>
2024-05-27 13:47:20 -0300

    Finalizado Ex017

--- ex_python_brasil/2_estrutura_de_decisao/017_verificador_de_ano_bissexto.py
+++ ex_python_brasil/2_estrutura_de_decisao/017_verificador_de_ano_bissexto.py
@@ -1,1 +1,9 @@
 # Faça um Programa que peça um número correspondente a um determinado ano e em seguida informe se este ano é ou não bissexto.
+
+print('Ano Bissexto')
+ano = int(input('Informe um ano para verificação: '))
+
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    print('O ano informado é bissexto!')
+else:
+    print('O ano informado não é bissexto!')
