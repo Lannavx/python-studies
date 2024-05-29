b6905874fc513404d33c4b09cf8110bea3552b61
Lanna Vieira <lanna_148@hotmail.com>
2024-05-28 22:43:02 -0300

    Finalizado Ex022

--- ex_python_brasil/2_estrutura_de_decisao/022_par_ou_impar.py
+++ ex_python_brasil/2_estrutura_de_decisao/022_par_ou_impar.py
@@ -1,1 +1,8 @@
 # Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão).
+
+numero = int(input('Informe um número inteiro qualquer: '))
+
+if numero % 2 == 0:
+    print(f'O número {numero} é PAR!')
+else:
+    print(f'O número {numero} é IMPAR!')    
