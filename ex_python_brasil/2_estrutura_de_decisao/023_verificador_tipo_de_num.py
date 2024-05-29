e9c597aee68553b974b1912be85a2de06f75e554
Lanna Vieira <lanna_148@hotmail.com>
2024-05-28 22:55:50 -0300

    Finalizado Ex023

--- ex_python_brasil/2_estrutura_de_decisao/023_verificador_tipo_de_num.py
+++ ex_python_brasil/2_estrutura_de_decisao/023_verificador_tipo_de_num.py
@@ -1,1 +1,9 @@
 # Faça um Programa que peça um número e informe se o número é inteiro ou decimal. Dica: utilize uma função de arredondamento.
+
+numero = float(input('Informe um número: '))
+
+# Compara o número original com sua versão arredondada
+if numero == round(numero):
+    print(f'O número {numero} é inteiro.')
+else:
+    print(f'O número {numero} é decimal.')
