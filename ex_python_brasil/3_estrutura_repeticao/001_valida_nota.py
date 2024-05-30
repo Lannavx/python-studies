e4e90cc947b050b220d2411a6301f1733057e90c
Lanna Vieira <lanna_148@hotmail.com>
2024-05-30 13:28:22 -0300

    Finalizado Ex001

--- ex_python_brasil/3_estrutura_repeticao/001_valida_nota.py
+++ ex_python_brasil/3_estrutura_repeticao/001_valida_nota.py
@@ -1,1 +1,11 @@
-# Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+''' Faça um programa que peça uma nota entre zero e dez. 
+Mostre uma mensagem caso o valor seja inválido e continue pedindo até que
+o usuário informe um valor válido.'''
+
+nota = float(input('Informe uma nota entre zero e dez: '))
+
+# Continua solicitando a nota enquanto ela estiver fora do intervalo permitido
+while nota < 0 or nota > 10:
+    nota = float(input('Valor incorreto! Informe uma nota entre zero e dez: '))
+
+print(f'Você informou a nota {nota}.')
