4f649c705f335f4af90da2250eb4d55bbf794b01
Lanna Vieira <lanna_148@hotmail.com>
2024-05-30 19:39:00 -0300

    Finalizado Ex002

--- ex_python_brasil/3_estrutura_repeticao/002_valida_senha.py
+++ ex_python_brasil/3_estrutura_repeticao/002_valida_senha.py
@@ -1,1 +1,12 @@
-# Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+'''Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha
+ igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.'''
+
+nome_usuario = input('Informe o nome de usuário: ')
+senha = input('Senha: ')
+
+# Continua solicitando uma nova senha enquanto ela for igual ao nome de usuário
+while senha == nome_usuario:
+    print('Senha não pode ser igual ao nome de usuário. Por favor, informe uma nova senha.')
+    senha = input('Senha: ')
+
+print('Nome de usuário e senha registrados com sucesso!')
