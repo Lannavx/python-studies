e665c4b792328df8e9ac407358e68332260d780a
Lanna Vieira <lanna_148@hotmail.com>
2024-05-31 15:26:28 -0300

    Finalizado Ex006

--- ex_python_brasil/3_estrutura_repeticao/006_imprime_numeros_vertical.py
+++ ex_python_brasil/3_estrutura_repeticao/006_imprime_numeros_vertical.py
@@ -1,1 +1,14 @@
-# Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro.
+''' Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+Depois modifique o programa para que ele mostre os números um ao lado do outro.'''
+
+print('Números na Vertical')  
+for numeros in range(1,21):
+    print(numeros) 
+
+print('-' * 5)    
+
+print('Números na Horizontal')  
+for numeros in range(1, 21):
+    print(numeros, end='')
+    # Verifica se é o último número para decidir se coloca ponto ou traço
+    print(' - ' if numeros < 20 else '.', end='')
