0ff9e1bfe22311bf3d48029c13f1145c9b812f38
Lanna Vieira <lanna_148@hotmail.com>
2024-06-01 16:34:36 -0300

    Finalizado Ex010

--- ex_python_brasil/3_estrutura_repeticao/010_imprime_impares.py
+++
@@ -1,1 +0,0 @@
-# Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.
---
+++ ex_python_brasil/3_estrutura_repeticao/010_intervalo_numeros.py
@@ -0,0 +1,21 @@
+# Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.
+
+num_um = int(input('Informe um número: '))
+num_dois = int(input('Informe outro número: '))
+
+# Ajusta os valores de início e fim para excluir os números inseridos e mostrar apenas o intervalo
+inicio = min(num_um, num_dois) + 1
+fim = max(num_um, num_dois) - 1
+
+print('-' * 30)
+print(f'O intervalo de números entre {num_um} e {num_dois} é: ')
+
+# Verifica se existe algum número no intervalo para imprimir
+if inicio <= fim:
+    # Usa o laço for para imprimir diretamente cada número no intervalo
+    for numeros in range(inicio, fim + 1):
+        print(numeros, end='')
+        print(' - ' if numeros != fim else '.', end='')
+else:
+    print("Não há números no intervalo entre os valores inseridos.")
+
