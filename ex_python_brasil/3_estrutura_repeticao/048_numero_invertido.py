01478e3d39ce758185dbc2a2a192f4476cd71cc7
Lanna Vieira <lanna_148@hotmail.com>
2024-06-20 14:49:09 -0300

    Finalizado Ex048

--- ex_python_brasil/3_estrutura_repeticao/048_numero_invertido.py
+++ ex_python_brasil/3_estrutura_repeticao/048_numero_invertido.py
@@ -1,4 +1,26 @@
 '''Faça um programa que peça um numero inteiro positivo e em seguida mostre este numero invertido.
+
 Exemplo:
   12376489
   => 98467321'''
+
+# Lê um número inteiro positivo
+numero = int(input('Informe um número inteiro positivo: '))
+
+# Verifica se o número é negativo
+while numero < 0:
+    numero = int(input('Por favor, informe um número inteiro POSITIVO: '))
+
+# Converte o número para string para facilitar a manipulação dos dígitos
+numero_str = str(numero)
+
+# Inicia a string que conterá o número invertido
+numero_invertido = ''
+
+# Laço para inverter os dígitos do número
+for digito in numero_str:
+    numero_invertido = digito + numero_invertido
+
+# Exibe o resultado
+print(f'{numero}')
+print(f'=> {numero_invertido}')
