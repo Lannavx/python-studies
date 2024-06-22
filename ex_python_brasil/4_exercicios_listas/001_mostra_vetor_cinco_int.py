1c6619f45526953b9397886a09229977e685989a
Lanna Vieira <lanna_148@hotmail.com>
2024-06-22 07:41:03 -0300

    Finalizado Ex001

--- ex_python_brasil/4_exercicios_listas/001_mostra_vetor_cinco_int.py
+++ ex_python_brasil/4_exercicios_listas/001_mostra_vetor_cinco_int.py
@@ -1,1 +1,17 @@
 # Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+
+# Importa o módulo random para gerar números aleatórios
+import random
+
+# Inicializa uma lista vazia para armazenar os números
+lista = []
+
+# Loop que itera 5 vezes, gerando 5 números
+for numeros in range(5):
+
+    # Gera um número aleatório entre 0 e 5 e os adiciona a lista
+    numeros = random.randrange(0, 6)
+    lista.append(numeros)
+
+# Exibe os números
+print(lista)
