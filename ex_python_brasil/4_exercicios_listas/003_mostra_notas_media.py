7abe3a42d7baf28726cb7f7fbbf8e378d2e76ba4
Lanna Vieira <lanna_148@hotmail.com>
2024-06-22 08:04:56 -0300

    Finalizado Ex003

--- ex_python_brasil/4_exercicios_listas/003_mostra_notas_media.py
+++ ex_python_brasil/4_exercicios_listas/003_mostra_notas_media.py
@@ -1,1 +1,18 @@
 # Faça um Programa que leia 4 notas, mostre as notas e a média na tela.
+
+# Inicializa uma lista vazia para armazenar as notas e define o número de notas a serem lidas
+lista_notas = []
+num_notas = 4
+
+# Loop que coleta as notas do usuário e adiciona a nota inserida à lista
+for nota in range(num_notas):
+    notas = float(input(f'Informe a {nota + 1}ª nota: '))
+
+    lista_notas.append(notas)
+
+# Calcula a média das notas na lista
+media = sum(lista_notas) / num_notas
+
+# Exibe os resultados
+print(f'\nAs notas são: {lista_notas}')
+print(f'E a média é de {media:.2f}')
