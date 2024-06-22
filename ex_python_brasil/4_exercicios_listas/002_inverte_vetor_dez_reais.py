5e9f6152ed7a9e00d03f92e3ed740c3673582195
Lanna Vieira <lanna_148@hotmail.com>
2024-06-22 07:52:29 -0300

    Finalizado Ex002

--- ex_python_brasil/4_exercicios_listas/002_inverte_vetor_dez_reais.py
+++ ex_python_brasil/4_exercicios_listas/002_inverte_vetor_dez_reais.py
@@ -1,1 +1,11 @@
 # Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Exibe a lista original
+print(f'Números: {lista}')
+
+lista.reverse()
+
+# Exibe a lista após ser invertida
+print(f'Números invertidos: {lista}')
