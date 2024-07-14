fbd22f3c1a2e02274cc15474613c1bf3fbf7cc14
Lanna Vieira <lanna_148@hotmail.com>
2024-07-14 15:07:42 -0300

    Finalizado Ex009

--- ex_python_brasil/4_exercicios_listas/009_soma_quadrados_vetor.py
+++ ex_python_brasil/4_exercicios_listas/009_soma_quadrados_vetor.py
@@ -1,1 +1,12 @@
 # Faça um Programa que leia um vetor A com 10 números inteiros, calcule e mostre a soma dos quadrados dos elementos do vetor.
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  
+
+# Inicializa a variável que armazenará a soma dos quadrados 
+soma_dos_quadrados = 0
+
+for numero in lista:
+    # Cada número é elevado ao quadrado e somado à variável
+    soma_dos_quadrados += numero ** 2
+
+print(f'A soma dos quadrados dos elementos do vetor é de {soma_dos_quadrados}')
