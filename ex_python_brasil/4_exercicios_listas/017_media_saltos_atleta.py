3da0b71b6fbf03e23a734bf53a88a2b67244d647
Lanna Vieira <lanna_148@hotmail.com>
2024-08-03 17:30:56 -0300

    Finalizado Ex017

--- ex_python_brasil/4_exercicios_listas/017_media_saltos_atleta.py
+++ ex_python_brasil/4_exercicios_listas/017_media_saltos_atleta.py
@@ -1,4 +1,9 @@
-'''Em uma competição de salto em distância cada atleta tem direito a cinco saltos. O resultado do atleta será determinado pela média dos cinco valores restantes. Você deve fazer um programa que receba o nome e as cinco distâncias alcançadas pelo atleta em seus saltos e depois informe o nome, os saltos e a média dos saltos. O programa deve ser encerrado quando não for informado o nome do atleta. A saída do programa deve ser conforme o exemplo abaixo:
+'''Em uma competição de salto em distância cada atleta tem direito a cinco saltos. 
+O resultado do atleta será determinado pela média dos cinco valores restantes. 
+Você deve fazer um programa que receba o nome e as cinco distâncias alcançadas pelo atleta 
+em seus saltos e depois informe o nome, os saltos e a média dos saltos. 
+O programa deve ser encerrado quando não for informado o nome do atleta. 
+A saída do programa deve ser conforme o exemplo abaixo:
 Atleta: Rodrigo Curvêllo
  
 Primeiro Salto: 6.5 m
@@ -12,3 +17,33 @@ Atleta: Rodrigo Curvêllo
 Saltos: 6.5 - 6.1 - 6.2 - 5.4 - 5.3
 Média dos saltos: 5.9 m
 '''
+
+SALTOS = 5
+
+# Lista para facilitar a impressão organizada
+ordens_saltos = ['Primeiro', 'Segundo', 'Terceiro', 'Quarto', 'Quinto']
+
+# Loop principal que continua solicita novos atletas
+while True:
+    nome_atleta = input('Atleta: ').strip()
+
+    # Interrompe o loop no caso de nome não informado
+    if not nome_atleta:
+        print('Programa encerrado. Nenhum nome fornecido.')
+        break
+
+    # Lista para armazenar as distâncias dos saltos
+    distancias_saltos = [] 
+
+    # Loop para obter as distâncias dos saltos    
+    for i in range(SALTOS):
+        distancia = float(input(f'{ordens_saltos[i]} Salto: '))
+        distancias_saltos.append(distancia)
+
+    media_salto = sum(distancias_saltos) / SALTOS
+
+    print('\nResultado final:\n')
+    print(f'Atleta: {nome_atleta}')
+    print(f'Saltos: {' - '.join(map(str, distancias_saltos))}')
+    print(f'Média dos saltos: {media_salto:.1f} m\n')
+    
