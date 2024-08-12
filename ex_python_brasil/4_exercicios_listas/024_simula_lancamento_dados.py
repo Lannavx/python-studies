834afab193eedae6309f729910275674d00ef10e
Lanna Vieira <lanna_148@hotmail.com>
2024-08-12 19:10:08 -0300

    Finalizado Ex024

--- ex_python_brasil/4_exercicios_listas/024_simula_lancamento_dados.py
+++ ex_python_brasil/4_exercicios_listas/024_simula_lancamento_dados.py
@@ -1,2 +1,27 @@
-'''O arquivo de entrada deve ser lido uma única vez, e os dados armazenados em memória, caso sejam necessários, de forma a agilizar a execução do programa. A conversão da espaço ocupado em disco, de bytes para megabytes deverá ser feita através de uma função separada, que será chamada pelo programa principal. O cálculo do percentual de uso também deverá ser feito através de uma função, que será chamada pelo programa principal.
-Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene os resultados em um vetor . Depois, mostre quantas vezes cada valor foi conseguido. Dica: use um vetor de contadores(1-6) e uma função para gerar numeros aleatórios, simulando os lançamentos dos dados.'''
+'''Faça um programa que simule um lançamento de dados. 
+Lance o dado 100 vezes e armazene os resultados em um vetor . 
+Depois, mostre quantas vezes cada valor foi conseguido. 
+Dica: use um vetor de contadores(1-6) e uma função para gerar numeros aleatórios, simulando os lançamentos dos dados.'''
+
+from random import randint
+
+# Inicializa a lista com número e contagem
+numeros_contador =[[1,0], [2,0], [3,0], [4,0], [5,0], [6,0]]
+jogadas = []
+
+# Função para gerar números aleatórios de 1 a 6
+def gerar_numero_aleatorio():
+    return randint(1,6)
+
+# Simula 100 jogadas e atualiza contagens diretamente
+for _ in range(100):
+    jogada = gerar_numero_aleatorio()
+    jogadas.append(jogada)
+    numeros_contador[jogada - 1][1] += 1
+
+# Exibe os resultados
+print(f'Lances: {", ".join(map(str, jogadas))}')
+
+print(f'\nContagem de cada valor:\n') 
+for num, contagem in numeros_contador:
+    print(f'Número {num} foi obtido {contagem} vezes')
