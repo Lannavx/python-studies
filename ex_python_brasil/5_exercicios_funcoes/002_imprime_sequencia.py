2489fa0b5acb623573b6fbcf0cba9270331473a7
Lanna Vieira <lanna_148@hotmail.com>
2024-08-13 15:01:35 -0300

    Finalizado Ex002

--- ex_python_brasil/5_exercicios_funcoes/002_imprime_sequencia.py
+++ ex_python_brasil/5_exercicios_funcoes/002_imprime_sequencia.py
@@ -5,3 +5,17 @@
     .....
     1   2   3   ...  n
 para um n informado pelo usuário. Use uma função que receba um valor n inteiro imprima até a n-ésima linha.'''
+
+def imprimir_sequencia(n):
+    '''Imprime uma sequência crescente de números para cada linha, começando em 1 e 
+    aumentando até o valor de n'''
+    for i in range(1, n + 1): 
+        for num in range(1, i + 1):
+             print(num, end=' ') 
+        print()
+
+# Solicita ao usuário um número
+n = int(input('Informe um número: '))
+
+# Chama a função com o número informado pelo usuário
+imprimir_sequencia(n)   
