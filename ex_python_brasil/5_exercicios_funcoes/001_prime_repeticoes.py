5e23355285a177710550be901c4ab5084125ffaf
Lanna Vieira <lanna_148@hotmail.com>
2024-08-13 13:52:16 -0300

    Finalizado Ex001

--- ex_python_brasil/5_exercicios_funcoes/001_prime_repeticoes.py
+++ ex_python_brasil/5_exercicios_funcoes/001_prime_repeticoes.py
@@ -5,3 +5,15 @@
     .....
     n   n   n   n   n   n  ... n
 para um n informado pelo usuário. Use uma função que receba um valor n inteiro e imprima até a n-ésima linha.'''
+
+
+def imprimir_valor(n):
+    '''Imprime o número i, repetido i vezes, separado por espaço'''
+    for i in range(1, n + 1):
+        print((str(i) + ' ') * i)
+
+# Solicita ao usuário um número
+n = int(input('Informe um número: '))
+
+# Chama a função com o número informado pelo usuário
+imprimir_valor(n)   
