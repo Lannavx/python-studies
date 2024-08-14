85cf9695b4944362d18f6496d8ada16595edb41d
Lanna Vieira <lanna_148@hotmail.com>
2024-08-14 18:32:19 -0300

    Finalizado Ex004

--- ex_python_brasil/5_exercicios_funcoes/004_classifica_numero.py
+++ ex_python_brasil/5_exercicios_funcoes/004_classifica_numero.py
@@ -1,1 +1,17 @@
-# Faça um programa, com uma função que necessite de um argumento. A função retorna o valor de caractere ‘P’, se seu argumento for positivo, e ‘N’, se seu argumento for zero ou negativo.
+''' Faça um programa, com uma função que necessite de um argumento. 
+A função retorna o valor de caractere "P", se seu argumento for positivo, e "N", se seu argumento for zero ou negativo.'''
+
+import random
+
+def classificar_numero(numero):
+    '''Recebe um argumento e retorna "P" se o número for positivo, e "N" se for zero ou negativo'''
+    if numero > 0:
+        return 'P'
+    else:
+        return 'N'    
+
+# Gera um número inteiro aleatório entre -100 e 100
+numero = random.randint(-100, 100)
+
+# Chama a função e imprime o resultado
+print(f'O número gerado foi {numero} e ele é {classificar_numero(numero)}')
