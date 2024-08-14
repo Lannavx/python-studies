a3772c8b85fa4705fe7c9a7b5856bad1fcd5999f
Lanna Vieira <lanna_148@hotmail.com>
2024-08-14 17:53:39 -0300

    Finalizado Ex003

--- ex_python_brasil/5_exercicios_funcoes/003_soma_tres.py
+++ ex_python_brasil/5_exercicios_funcoes/003_soma_tres.py
@@ -1,1 +1,14 @@
 # Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
+
+def somar_valores(x,y,z):
+    '''Soma três valores e retorna o resultado'''
+    return x + y + z
+
+# Solicita ao usuário 3 números inteiros
+print('Informe três números inteiros para ver a soma deles.')
+x = int(input('Informe o 1º número: '))
+y = int(input('Informe o 2º número: '))
+z = int(input('Informe o 3º número: '))
+
+# Chama a função com os três números fornecidos e exibe o resultado
+print(f'A soma dos números é de {somar_valores(x, y, z)}')
