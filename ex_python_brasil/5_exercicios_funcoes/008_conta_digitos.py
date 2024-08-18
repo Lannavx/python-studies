ae9fc2afe35f1d5b0fdf3a1cb8d4ed88e253fb0a
Lanna Vieira <lanna_148@hotmail.com>
2024-08-18 17:23:27 -0300

    Finalizado Ex008

--- ex_python_brasil/5_exercicios_funcoes/008_conta_digitos.py
+++ ex_python_brasil/5_exercicios_funcoes/008_conta_digitos.py
@@ -1,1 +1,13 @@
 # Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado.
+
+def informar_quantidade(numero):
+    '''Conta os dígitos de um número inteiro'''
+    numero = abs(numero)  # Remove o sinal negativo se houver
+    return len(str(numero))
+
+
+# Solicita ao usuário um número inteiro
+numero = int(input('Digite um número inteiro: '))
+
+# Exibe o resultado
+print(f'A quantidade de dígitos é de {informar_quantidade(numero)}')
