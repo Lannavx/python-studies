9f6de19647faa4102152fdb5d58c2c59c43bdcf7
Lanna Vieira <lanna_148@hotmail.com>
2024-08-21 17:44:42 -0300

    Finalizado Ex002

--- ex_python_brasil/6_exercicios_strings/002_nome_inverso_maiusculas.py
+++ ex_python_brasil/6_exercicios_strings/002_nome_inverso_maiusculas.py
@@ -1,1 +1,13 @@
-'''Nome ao contrário em maiúsculas. Faça um programa que permita ao usuário digitar o seu nome e em seguida mostre o nome do usuário de trás para frente utilizando somente letras maiúsculas. Dica: lembre−se que ao informar o nome o usuário pode digitar letras maiúsculas ou minúsculas.'''
+'''Nome ao contrário em maiúsculas. 
+Faça um programa que permita ao usuário digitar o seu nome e em seguida mostre o nome do usuário de 
+trás para frente utilizando somente letras maiúsculas. 
+Dica: lembre−se que ao informar o nome o usuário pode digitar letras maiúsculas ou minúsculas.'''
+
+# Solicita ao usuário que digite um nome
+nome = input('Digite seu nome: ').strip().upper()
+
+# Inverte o nome
+nome_invertido = nome[::-1]
+
+# Exibe o resultado
+print(f'Seu nome de trás para frente é: {nome_invertido}')
