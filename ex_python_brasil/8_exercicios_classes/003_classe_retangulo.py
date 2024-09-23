cdb79a55086986ee77b825a28367d34f09d607dd
Lannavx <lanna_148@hotmail.com>
2024-09-23 15:45:56 -0300

    Finalizado Ex003

--- ex_python_brasil/8_exercicios_classes/003_classe_retangulo.py
+++ ex_python_brasil/8_exercicios_classes/003_classe_retangulo.py
@@ -3,3 +3,35 @@
 Atributos: LadoA, LadoB (ou Comprimento e Largura, ou Base e Altura, a escolher)
 a. Métodos: Mudar valor dos lados, Retornar valor dos lados, calcular Área e calcular Perímetro;
 b.Crie um programa que utilize esta classe. Ele deve pedir ao usuário que informe as medidades de um local. Depois, deve criar um objeto com as medidas e calcular a quantidade de pisos e de rodapés necessárias para o local.'''
+
+class Retangulo():
+    def __init__(self, comprimento: float, largura: float):
+        self.comprimento = comprimento
+        self.largura = largura
+
+    def mudar_valor_lados(self, novo_valor_cumprimento: float, 
+        novo_valor_largura: float):
+        self.comprimento = novo_valor_cumprimento
+        self.largura = novo_valor_largura
+
+    def retornar_valor_lados(self):
+       return self.comprimento, self.largura
+
+    def calcular_area(self):
+        return self.comprimento * self.largura
+
+    def calcular_perimetro(self):
+        return 2 * (self.comprimento + self.largura)         
+
+
+# Solicita as medidas do local ao usuário
+largura = float(input('Informe a largura do local: '))
+comprimento = float(input('Informe o comprimento do local: '))
+
+local = Retangulo(comprimento, largura)
+
+area_local = local.calcular_area()
+perimetro_local = local.calcular_perimetro()
+
+print(f'\nÁrea do local: {area_local:.2f} m²')
+print(f'Perímetro do local: {perimetro_local:.2f} m')
