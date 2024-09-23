93bb5843d9b5905a139d8c537399d2ce833e4242
Lannavx <lanna_148@hotmail.com>
2024-09-23 14:53:27 -0300

    Finalizado Ex002

--- ex_python_brasil/8_exercicios_classes/002_classe_quadrado.py
+++ ex_python_brasil/8_exercicios_classes/002_classe_quadrado.py
@@ -2,3 +2,24 @@
 
 a. Atributos: Tamanho do lado
 b. Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;'''
+
+class Quadrado():
+    def __init__(self, tamanho_lado: float):
+        self.tamanho_lado = tamanho_lado
+
+    def mudar_valor_lado(self, novo_valor: float):
+        self.tamanho_lado = novo_valor
+
+    def retornar_valor_lado(self):
+        return self.tamanho_lado
+
+    def calcular_area(self):
+         return self.tamanho_lado ** 2
+
+
+quadrado = Quadrado(4)
+
+quadrado.mudar_valor_lado(8)
+
+print(quadrado.retornar_valor_lado())
+print(quadrado.calcular_area())
